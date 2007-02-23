ff6c868ddb752a58a20b5749953d98b45572f33e
Robert M. Fuhrer <rmfuhrer@optonline.net>
2007-02-23 19:30:28 +0000

    Greatly simplified this class by using the symbol table support now integrated into the grammar.

--- src/org/eclipse/imp/lpg/parser/ASTUtils.java
+++ src/org/eclipse/imp/lpg/parser/ASTUtils.java
@@ -19,60 +19,24 @@ public class ASTUtils {
     }
 
     public static List<Imacro_name_symbol> getMacros(JikesPG root) {
-        List<Imacro_name_symbol> result= new ArrayList<Imacro_name_symbol>();
-    
+	JikesPGParser.SymbolTable st= root.symbolTable;
+
         // DO NOT pick up macros from any imported file! They shouldn't be treated as defined in this scope!
-        List<ASTNode> defineSegs= findItemOfType(root, DefineSeg.class);
-        for(Iterator iter= defineSegs.iterator(); iter.hasNext(); ) {
-	    DefineSeg defineSeg= (DefineSeg) iter.next();
-	    
-	    if (defineSeg == null) continue;
-
-	    defineSpecList defines= defineSeg.getdefine_segment();
-
-	    for(int i= 0; i < defines.size(); i++) {
-		defineSpec define= defines.getdefineSpecAt(i);
-		result.add(define.getmacro_name_symbol());
-	    }
-	}
-        return result;
+        return st.allDefsOfType(Imacro_name_symbol.class);
     }
 
     public static List<nonTerm> getNonTerminals(JikesPG root) {
-        List<nonTerm> result= new ArrayList<nonTerm>();
-    
-        // TODO: pick up non-terminals from any imported file
-        List<ASTNode> rulesSegs= findItemOfType(root, RulesSeg.class);
+	JikesPGParser.SymbolTable st= root.symbolTable;
 
-        if (rulesSegs.size() == 0) return Collections.emptyList();
-
-        for(Iterator iter= rulesSegs.iterator(); iter.hasNext(); ) {
-	    RulesSeg rulesSeg= (RulesSeg) iter.next();
-	    
-	    rules_segment rules_seg= rulesSeg.getrules_segment();
-	    nonTermList nonTermList= rules_seg.getnonTermList();
-	    
-	    result.addAll(nonTermList.getArrayList());
-	}
-        return result;
+        // TODO: pick up non-terminals from imported files
+        return st.allDefsOfType(nonTerm.class);
     }
 
     public static List<terminal> getTerminals(JikesPG root) {
-        List<terminal> result= new ArrayList();
-    
-        // TODO: pick up terminals from any imported file
-        List<ASTNode> terminalsSegs= findItemOfType(root, TerminalsSeg.class);
+	JikesPGParser.SymbolTable st= root.symbolTable;
 
-        for(Iterator iter= terminalsSegs.iterator(); iter.hasNext();) {
-	    TerminalsSeg terminalsSeg= (TerminalsSeg) iter.next();
-
-	    if (terminalsSeg != null) {
-	        terminalList terminals= terminalsSeg.getterminals_segment();
-	        
-	        result.addAll(terminals.getArrayList());
-	    }
-	}
-        return result;
+        // TODO: pick up terminals from imported files???
+        return st.allDefsOfType(terminal.class);
     }
 
     public static List<ASTNode> findItemOfType(JikesPG root, Class ofType) {
@@ -81,7 +45,7 @@ public class ASTUtils {
 
         for(int i=0; i < itemList.size(); i++) {
             IJikesPG_item item= itemList.getJikesPG_itemAt(i);
-    
+
             if (ofType.isInstance(item))
                 result.add((ASTNode) item);
         }
@@ -92,56 +56,31 @@ public class ASTUtils {
         // This would use the auto-generated bindings if they were implemented already...
         String id= HoverHelper.stripName(s.toString());
 
-        List<nonTerm> nonTermList= getNonTerminals(root);
-        List<terminal> termList= getTerminals(root);
-        List<Imacro_name_symbol> macros= getMacros(root);
-
-        for(int j=0; j < nonTermList.size(); j++) {
-            nonTerm nonTerm= nonTermList.get(j);
-            String nonTermName= HoverHelper.stripName(nonTerm.getSYMBOL().toString());
-
-            if (nonTermName.equals(id))
-        	return nonTerm;
-        }
-        for(int j=0; j < termList.size(); j++) {
-            terminal term= termList.get(j);
-            String termName= HoverHelper.stripName(term.getterminal_symbol().toString());
-
-            if (termName.equals(id))
-        	return term;
-        }
-        for(int j=0; j < macros.size(); j++) {
-            Imacro_name_symbol macro= macros.get(j);
-            String macroName= macro.toString();
-
-            if (macroName.equals(s.toString()))
-        	return (ASTNode) macro;
-        }
-        return null;
+        return root.symbolTable.lookup(id);
     }
 
     public static List<ASTNode> findRefsOf(final nonTerm nonTerm) {
 	final List<ASTNode> result= new ArrayList<ASTNode>();
 	JikesPG root= getRoot(nonTerm);
-
 	List<nonTerm> nonTerms= getNonTerminals(root);
 
 	// Indexed search would be nice here...
 	for(int i=0; i < nonTerms.size(); i++) {
 	    nonTerm nt= nonTerms.get(i);
+	    final String nonTermName= nonTerm.getruleNameWithAttributes().getSYMBOL().toString();
 
 	    nt.accept(new AbstractVisitor() {
 		public void unimplementedVisitor(String s) { }
 		public boolean visit(symWithAttrs1 n) {
-		    if (n.getSYMBOL().toString().equals(nonTerm.getSYMBOL().toString()))
+		    if (n.getSYMBOL().toString().equals(nonTermName))
 			result.add(n);
 		    return super.visit(n);
 		}
-		public boolean visit(symWithAttrs2 n) {
-		    if (n.getSYMBOL().toString().equals(nonTerm.getSYMBOL().toString()))
-			result.add(n);
-		    return super.visit(n);
-		}
+//		public boolean visit(symWithAttrs2 n) {
+//		    if (n.getSYMBOL().toString().equals(nonTermName))
+//			result.add(n);
+//		    return super.visit(n);
+//		}
 	    });
 	}
 	return result;
