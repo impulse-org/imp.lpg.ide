ecfe3cb9f88f2924812ccbe9a7ebf9721a23ebbe
Robert M. Fuhrer <rmfuhrer@optonline.net>
2007-08-10 18:58:34 +0000

    Added trivial (semi-useless) implementation of "mark occurrences".

---
+++ src/org/eclipse/imp/lpg/editor/LPGOccurrenceMarker.java
@@ -0,0 +1,37 @@
+package org.eclipse.imp.lpg.editor;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.imp.editor.IOccurrenceMarker;
+import org.eclipse.imp.lpg.parser.ASTUtils;
+import org.eclipse.imp.lpg.parser.LPGParser.ASTNode;
+import org.eclipse.imp.lpg.parser.LPGParser.IASTNodeToken;
+import org.eclipse.imp.lpg.parser.LPGParser.JikesPG;
+import org.eclipse.imp.parser.IParseController;
+
+public class LPGOccurrenceMarker implements IOccurrenceMarker {
+
+    public LPGOccurrenceMarker() {
+    }
+
+    public String getKindName() {
+	return "References";
+    }
+
+    public List<Object> getOccurrencesOf(IParseController parseController, Object entity) {
+	ASTNode node= (ASTNode) entity;
+	JikesPG root= (JikesPG) parseController.getCurrentAst();
+	List<Object> result;
+
+	System.out.println();
+	if (node instanceof IASTNodeToken) {
+	    IASTNodeToken nTok= (IASTNodeToken) node;
+	    result= Collections.singletonList(ASTUtils.findDefOf(nTok, root, parseController));
+	} else {
+	    result= Collections.emptyList();
+	}
+	System.out.println("LPG occurrences = " + result);
+	return result;
+    }
+}
