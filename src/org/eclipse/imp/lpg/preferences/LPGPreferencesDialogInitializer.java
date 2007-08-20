e46caf4847f425708d98c022134e85bcd63f3224
Stanley M. Sutton <suttons@us.ibm.com>
2007-08-20 21:12:10 +0000

    Added method to initialize default include path.

--- src/org/eclipse/imp/lpg/preferences/LPGPreferencesDialogInitializer.java
+++ src/org/eclipse/imp/lpg/preferences/LPGPreferencesDialogInitializer.java
@@ -1,9 +1,14 @@
 package org.eclipse.imp.lpg.preferences;
 
+import java.io.IOException;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.imp.lpg.LPGRuntimePlugin;
 import org.eclipse.imp.lpg.builder.LPGBuilder;
 import org.eclipse.imp.preferences.IPreferencesService;
+import org.osgi.framework.Bundle;
 
 /**
  * Initializations of default values for preferences.
@@ -21,17 +26,31 @@ public class LPGPreferencesDialogInitializer extends AbstractPreferenceInitializ
 		service.setBooleanPreference(IPreferencesService.DEFAULT_LEVEL, LPGPreferencesDialogConstants.P_USEDEFAULTEXECUTABLE, true);
 		service.setStringPreference(IPreferencesService.DEFAULT_LEVEL, LPGPreferencesDialogConstants.P_EXECUTABLETOUSE, getExecutableToUse());					//"C:\\eclipse\\workspaces\\eclipse-3.2.2-2\\lpg.runtime.java\\lpgexe\\lpg-win32_x86.exe");
 		service.setBooleanPreference(IPreferencesService.DEFAULT_LEVEL, LPGPreferencesDialogConstants.P_USEDEFAULTINCLUDEPATH, true);
-		service.setStringPreference(IPreferencesService.DEFAULT_LEVEL, LPGPreferencesDialogConstants.P_INCLUDEPATHTOUSE, ".;..;");
+		service.setStringPreference(IPreferencesService.DEFAULT_LEVEL, LPGPreferencesDialogConstants.P_INCLUDEPATHTOUSE, getDefaultIncludePath());
 		service.setStringPreference(IPreferencesService.DEFAULT_LEVEL, LPGPreferencesDialogConstants.P_SOURCEFILEEXTENSIONS, "g");
 		service.setStringPreference(IPreferencesService.DEFAULT_LEVEL, LPGPreferencesDialogConstants.P_INCLUDEFILEEXTENSIONS, "gi");
 		service.setBooleanPreference(IPreferencesService.DEFAULT_LEVEL, LPGPreferencesDialogConstants.P_EMITDIAGNOSTICS, true);
 		service.setBooleanPreference(IPreferencesService.DEFAULT_LEVEL, LPGPreferencesDialogConstants.P_GENERATELISTINGS, true);
 	}
 	
-	
+			
 	public String getExecutableToUse() {
 		return LPGBuilder.getDefaultExecutablePath();
 	}
 	
 	
+    public static String getDefaultIncludePath() {
+    	Bundle bundle= Platform.getBundle(LPGBuilder.LPG_PLUGIN_ID);
+    	try {
+    	    // Use getEntry() rather than getResource(), since the "templates" folder is
+    	    // no longer inside the plugin jar (which is now expanded upon installation).
+    	    String tmplPath= FileLocator.toFileURL(bundle.getEntry("templates")).getFile();
+
+    	    if (Platform.getOS().equals("win32"))
+    		tmplPath= tmplPath.substring(1);
+    	    return tmplPath;
+    	} catch(IOException e) {
+    	    return null;
+    	}
+    }
 }
