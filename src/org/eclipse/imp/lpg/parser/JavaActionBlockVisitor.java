fa49a2b7462912a32e2634da5d9f12de760e642e
Philippe Charles <pgc1@rice.edu>
2007-11-05 22:22:06 +0000

    Check in JavaActionBlockVisitor used for parsing embedded Java in lpg source

---
+++ src/org/eclipse/imp/lpg/parser/JavaActionBlockVisitor.java
@@ -0,0 +1,82 @@
+package org.eclipse.imp.lpg.parser;
+
+import org.eclipse.imp.lpg.parser.LPGParser.*;
+
+public class JavaActionBlockVisitor extends AbstractVisitor
+{
+    private JavaLexer javaLexer = new JavaLexer(); // lexer can be shared.
+    private JavaParser javaParser;
+
+    public void initialize(LPGParser env)
+    {
+        javaLexer.initialize(env.getInputChars(), env.getFileName());
+        javaParser = new JavaParser(javaLexer); // allocate new parser (and parse stream).
+        javaParser.setMessageHandler(env.getMessageHandler());
+    }
+         
+    private void parseClassBodyDeclarationsopt(action_segmentList list) {
+        for (int i = 0; i < list.size(); i++)
+            parseClassBodyDeclarationsopt((action_segment) list.getaction_segmentAt(i));
+    }
+        
+    private void parseClassBodyDeclarationsopt(action_segment n) {
+        int start_offset = n.getBLOCK().getStartOffset() + 2,
+            end_offset   = n.getBLOCK().getEndOffset() - 2;
+        javaParser.getParseStream().resetTokenStream();
+        javaLexer.lexer(javaParser, start_offset, end_offset);
+        n.setAst(javaParser.parseClassBodyDeclarationsopt());
+    }
+
+    private void parse(action_segmentList list) {
+        for (int i = 0; i < list.size(); i++)
+            parse((action_segment) list.getaction_segmentAt(i));
+    }
+   
+    private void parse(action_segment n) {
+        int start_offset = n.getBLOCK().getStartOffset() + 2,
+            end_offset   = n.getBLOCK().getEndOffset() - 2;
+        javaParser.getParseStream().resetTokenStream();
+        javaLexer.lexer(javaParser, start_offset, end_offset);
+        n.setAst(javaParser.parser());
+    }
+
+    //
+    // The visitors
+    //
+    public void unimplementedVisitor(String s) {
+        // Sometimes useful for debugging
+        //System.out.println(s);
+    }
+
+    public boolean visit(AstSeg n) {
+        parseClassBodyDeclarationsopt(n.getast_segment());
+        return false;
+    }
+
+    public boolean visit(GlobalsSeg n) {
+        parse(n.getglobals_segment());
+        return false;
+    }
+
+    public boolean visit(HeadersSeg n) {
+        parseClassBodyDeclarationsopt(n.getheaders_segment());
+        return false;
+    }
+
+    public boolean visit(NoticeSeg n) {
+        parse(n.getnotice_segment());
+        return false;
+    }
+
+    public boolean visit(TrailersSeg n) {
+         parseClassBodyDeclarationsopt(n.gettrailers_segment());
+         return false;
+    }
+
+    public boolean visit(rule n) {
+        action_segment a = n.getopt_action_segment();
+        if (a != null)
+            parseClassBodyDeclarationsopt(a);
+        return false;
+    }
+}
