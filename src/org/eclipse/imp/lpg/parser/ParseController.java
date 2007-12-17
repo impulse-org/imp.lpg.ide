c5725401e62b22c647dcc1dd0e1d938b5551998c
Robert M. Fuhrer <rmfuhrer@optonline.net>
2007-12-17 19:01:23 +0000

    Accommodate new IParseController API that is no longer dependent on LPG-specific types. Now derives from SimpleLPGParseController, which permitted the removal of much common code.

--- src/org/eclipse/imp/lpg/parser/ParseController.java
+++ src/org/eclipse/imp/lpg/parser/ParseController.java
@@ -3,23 +3,15 @@
  * 
  * This file is part of the Eclipse IMP.
  */
-/*
- * Created on Oct 28, 2005
- */
 package org.eclipse.imp.lpg.parser;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-
-import lpg.runtime.IMessageHandler;
-import lpg.runtime.IToken;
-import lpg.runtime.Monitor;
 
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.imp.language.ILanguageSyntaxProperties;
 import org.eclipse.imp.lpg.builder.LPGBuilder;
 import org.eclipse.imp.lpg.parser.LPGParser.ASTNode;
 import org.eclipse.imp.lpg.parser.LPGParser.IASTNodeToken;
@@ -30,235 +22,146 @@ import org.eclipse.imp.lpg.parser.LPGParser.optionList;
 import org.eclipse.imp.lpg.parser.LPGParser.option_spec;
 import org.eclipse.imp.lpg.parser.LPGParser.option_specList;
 import org.eclipse.imp.lpg.parser.LPGParser.option_value0;
-import org.eclipse.imp.model.ICompilationUnit;
 import org.eclipse.imp.model.ISourceProject;
-import org.eclipse.imp.model.ModelFactory;
-import org.eclipse.imp.parser.IASTNodeLocator;
 import org.eclipse.imp.parser.ILexer;
+import org.eclipse.imp.parser.IMessageHandler;
 import org.eclipse.imp.parser.IParseController;
 import org.eclipse.imp.parser.IParser;
-import org.eclipse.imp.parser.ParseError;
+import org.eclipse.imp.parser.ISourcePositionLocator;
+import org.eclipse.imp.parser.MessageHandlerAdapter;
+import org.eclipse.imp.parser.SimpleLPGParseController;
 
-public class ParseController implements IParseController {
-    private IPath fFilePath;
-    private ISourceProject fProject;
-    private IMessageHandler fHandler;
+public class ParseController extends SimpleLPGParseController implements IParseController {
     private LPGParser fParser;
+
     private LPGLexer fLexer;
-    private ASTNode fCurrentAst;
+
     private JavaActionBlockVisitor actionVisitor;
-    private char fKeywords[][];
-    private boolean fIsKeyword[];
+
+    public ParseController() {
+        fLexer= new LPGLexer(); // Create the lexer
+        fParser= new LPGParser();
+    }
 
     public void initialize(IPath filePath, ISourceProject project, IMessageHandler handler) {
-	fFilePath= filePath;
-	fProject= project;
-	fHandler= handler;
-    }
-
-    public ISourceProject getProject() {
-	return fProject;
-    }
-
-    public IPath getPath() {
-	return fFilePath;
+        super.initialize(filePath, project, handler);
     }
 
     public IParser getParser() {
-	return fParser;
+        return fParser;
     }
 
     public ILexer getLexer() {
-	return fLexer;
+        return fLexer;
     }
 
-    public Object getCurrentAst() {
-	return fCurrentAst;
+    public ISourcePositionLocator getNodeLocator() {
+        return new NodeLocator(this);
     }
 
-    public char[][] getKeywords() {
-	return fKeywords;
-    }
-
-    public boolean isKeyword(int kind) {
-	return fIsKeyword[kind];
-    }
-
-    public String getSingleLineCommentPrefix() {
-    	return "--";
-    }
-    
-    public int getTokenIndexAtCharacter(int offset) {
-	int index= fParser.getParseStream().getTokenIndexAtCharacter(offset);
-	return (index < 0 ? -index : index);
-    }
-
-    public IToken getTokenAtCharacter(int offset) {
-	return fParser.getParseStream().getTokenAtCharacter(offset);
-    }
-
-    public IASTNodeLocator getNodeLocator() {
-	return new NodeLocator(this);
-    }
-
-    public boolean hasErrors() {
-	return fCurrentAst == null;
-    }
-
-    public List getErrors() {
-	return Collections.singletonList(new ParseError("parse error", null));
-    }
-
-    public ParseController() {
-	fLexer= new LPGLexer(); // Create the lexer
-	fParser= new LPGParser();
-    }
-
-    class MyMonitor implements Monitor {
-	IProgressMonitor monitor;
-	boolean wasCancelled= false;
-
-	MyMonitor(IProgressMonitor monitor) {
-	    this.monitor= monitor;
-	}
-
-	public boolean isCancelled() {
-	    if (!wasCancelled)
-		wasCancelled= monitor.isCanceled();
-	    return wasCancelled;
-	}
+    public ILanguageSyntaxProperties getSyntaxProperties() {
+        return new LPGSyntaxProperties();
     }
 
     public static List<option> getOptions(JikesPG root) {
-        List<option> result = new ArrayList<option>();
-        String template_file = null;
-        option_specList optSeg = root.getoptions_segment();
-        for(int i = 0; i < optSeg.size(); i++) {
-            option_spec optSpec = optSeg.getoption_specAt(i);
-            optionList optList = optSpec.getoption_list();
-            for(int o = 0; o < optList.size(); o++) {
-                option opt = optList.getoptionAt(o);
+        List<option> result= new ArrayList<option>();
+        String template_file= null;
+        option_specList optSeg= root.getoptions_segment();
+        for(int i= 0; i < optSeg.size(); i++) {
+            option_spec optSpec= optSeg.getoption_specAt(i);
+            optionList optList= optSpec.getoption_list();
+            for(int o= 0; o < optList.size(); o++) {
+                option opt= optList.getoptionAt(o);
                 result.add(opt);
-                IASTNodeToken sym = opt.getSYMBOL();
-                String optName = sym.toString();
+                IASTNodeToken sym= opt.getSYMBOL();
+                String optName= sym.toString();
                 if (optName.equals("template")) {
-                    Ioption_value optValue = opt.getoption_value();
+                    Ioption_value optValue= opt.getoption_value();
                     if (optValue instanceof option_value0)
-                        template_file = ((option_value0) optValue).getSYMBOL().toString();
+                        template_file= ((option_value0) optValue).getSYMBOL().toString();
                 }
             }
         }
-        
+
         if (template_file != null) {
-            String include_str = LPGBuilder.getDefaultIncludePath();
-            int offset,
-                i = -1;
+            String include_str= LPGBuilder.getDefaultIncludePath();
+            int offset, i= -1;
             do {
-                offset = i + 1;
-                i = include_str.indexOf(';', offset);
-                String filename = include_str.substring(offset, i == -1 ? include_str.length() : i) + template_file;
-                File f = new File(filename);
+                offset= i + 1;
+                i= include_str.indexOf(';', offset);
+                String filename= include_str.substring(offset, i == -1 ? include_str.length() : i) + template_file;
+                File f= new File(filename);
                 if (f.exists()) {
                     try {
-                        LPGLexer lex = new LPGLexer(filename);
-                        LPGParser prs = new LPGParser(lex.getLexStream()); // Create the parser
-                        lex.lexer(prs.getParseStream()); // Lex the stream to produce the token stream
-                        JikesPG template_root = (JikesPG) prs.parser(); // Parse the token stream to produce an AST
+                        LPGLexer lex= new LPGLexer(filename);
+                        LPGParser prs= new LPGParser(lex.getLexStream()); // Create
+                                                                            // the
+                                                                            // parser
+                        lex.lexer(prs.getParseStream()); // Lex the stream to
+                                                            // produce the token
+                                                            // stream
+                        JikesPG template_root= (JikesPG) prs.parser(); // Parse
+                                                                        // the
+                                                                        // token
+                                                                        // stream
+                                                                        // to
+                                                                        // produce
+                                                                        // an
+                                                                        // AST
                         if (template_root != null) {
                             result.addAll(getOptions(template_root));
                             break;
                         }
+                    } catch (java.io.IOException e) {
+                        // skip this file
                     }
-                    catch (java.io.IOException e) {
-                            // skip this file            		
-                    }
-                } 
+                }
             } while (i != -1);
         }
-        
+
         return result;
     }
 
     public Object parse(String contents, boolean scanOnly, IProgressMonitor monitor) {
-	MyMonitor my_monitor= new MyMonitor(monitor);
-	char[] contentsArray= contents.toCharArray();
+        PMMonitor my_monitor= new PMMonitor(monitor);
+        char[] contentsArray= contents.toCharArray();
 
-	fLexer.reset(contentsArray, fFilePath.toOSString());
-	fParser.reset(fLexer.getLexStream());
-	fParser.getParseStream().setMessageHandler(fHandler);
-	fLexer.lexer(my_monitor, fParser.getParseStream()); // Lex the stream to produce the token stream
-	if (my_monitor.isCancelled())
-	    return fCurrentAst; // TODO fCurrentAst might (probably will) be inconsistent wrt the lex stream now
-	fCurrentAst= (ASTNode) fParser.parser(my_monitor, 0);
-	if (fCurrentAst == null)
-	    fParser.getParseStream().dumpTokens();
-	else {
-        boolean is_java = false,
-                automatic_ast = false;
-        for(option opt : getOptions((JikesPG) fCurrentAst)) {
-            IASTNodeToken sym = opt.getSYMBOL();
-            String optName= sym.toString();
-            if (optName.equalsIgnoreCase("programming-language") ||
-                optName.equalsIgnoreCase("programming_language") ||
-                optName.equalsIgnoreCase("programminglanguage") ||
-                optName.equalsIgnoreCase("table")) {
-                Ioption_value optValue = opt.getoption_value();
-                if (optValue instanceof option_value0)
-                    is_java = ((option_value0) optValue).getSYMBOL().toString().equalsIgnoreCase("java");
+        fLexer.reset(contentsArray, fFilePath.toOSString());
+        fParser.reset(fLexer.getLexStream());
+        fParser.getParseStream().setMessageHandler(new MessageHandlerAdapter(handler));
+        fLexer.lexer(my_monitor, fParser.getParseStream()); // Lex the stream to
+                                                            // produce the token
+                                                            // stream
+        if (my_monitor.isCancelled())
+            return fCurrentAst; // TODO fCurrentAst might (probably will) be
+                                // inconsistent wrt the lex stream now
+        fCurrentAst= (ASTNode) fParser.parser(my_monitor, 0);
+        if (fCurrentAst == null)
+            fParser.getParseStream().dumpTokens();
+        else {
+            boolean is_java= false, automatic_ast= false;
+            for(option opt : getOptions((JikesPG) fCurrentAst)) {
+                IASTNodeToken sym= opt.getSYMBOL();
+                String optName= sym.toString();
+                if (optName.equalsIgnoreCase("programming-language") || optName.equalsIgnoreCase("programming_language")
+                        || optName.equalsIgnoreCase("programminglanguage") || optName.equalsIgnoreCase("table")) {
+                    Ioption_value optValue= opt.getoption_value();
+                    if (optValue instanceof option_value0)
+                        is_java= ((option_value0) optValue).getSYMBOL().toString().equalsIgnoreCase("java");
+                } else if (optName.equalsIgnoreCase("automatic-ast") || optName.equalsIgnoreCase("automatic_ast") || optName.equalsIgnoreCase("automaticast"))
+                    automatic_ast= true;
+                else if (optName.equalsIgnoreCase("noautomatic-ast") || optName.equalsIgnoreCase("noautomatic_ast")
+                        || optName.equalsIgnoreCase("noautomaticast"))
+                    automatic_ast= false;
             }
-            else if (optName.equalsIgnoreCase("automatic-ast") ||
-                     optName.equalsIgnoreCase("automatic_ast") ||
-                     optName.equalsIgnoreCase("automaticast"))
-                automatic_ast = true;
-            else if (optName.equalsIgnoreCase("noautomatic-ast") ||
-                     optName.equalsIgnoreCase("noautomatic_ast") ||
-                     optName.equalsIgnoreCase("noautomaticast"))
-            automatic_ast = false;
+
+            if (is_java) {
+                actionVisitor= (automatic_ast ? new JavaActionBlockAutomaticVisitor() : new JavaActionBlockUserDefinedVisitor());
+                actionVisitor.reset(fParser);
+                ((ASTNode) fCurrentAst).accept(actionVisitor);
+            }
         }
-    
-        if (is_java)
-        {        
-            actionVisitor = (automatic_ast ? new JavaActionBlockAutomaticVisitor()
-            		                       : new JavaActionBlockUserDefinedVisitor());
-            actionVisitor.reset(fParser);
-            fCurrentAst.accept(actionVisitor);
-        }
+        cacheKeywordsOnce();
+        return fCurrentAst;
     }
-	if (fKeywords == null)
-	    initKeywords();
-	return fCurrentAst;
-    }
-
-    private void initKeywords() {
-	String tokenKindNames[]= fParser.orderedTerminalSymbols();
-
-	this.fIsKeyword= new boolean[tokenKindNames.length];
-	this.fKeywords= new char[tokenKindNames.length][];
-
-	int[] keywordKinds= fLexer.getKeywordKinds();
-
-	for(int i= 1; i < keywordKinds.length; i++) {
-	    int index= fParser.getParseStream().mapKind(keywordKinds[i]);
-	    fIsKeyword[index]= true;
-	    fKeywords[index]= fParser.orderedTerminalSymbols()[index].toCharArray();
-	}
-    }
-    
-    /*
-     * For the management of associated problem-marker types
-     */
-    
-    private static List problemMarkerTypes = new ArrayList();
-    
-    public List getProblemMarkerTypes() {
-    	return problemMarkerTypes;
-    }
-    
-    public void addProblemMarkerType(String problemMarkerType) {
-    	problemMarkerTypes.add(problemMarkerType);
-    }
-    
-	public void removeProblemMarkerType(String problemMarkerType) {
-		problemMarkerTypes.remove(problemMarkerType);
-	}
 }
