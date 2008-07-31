86a62e3d032b6239365c1204d9b8d8cd72cb7cb2
Stanley M. Sutton <suttons@us.ibm.com>
2008-07-31 03:12:08 +0000

    Beefed up substantially to mark all occurrences of a nonterminal (in addition to the definition) and to mark all occurrences of terminals (including in both the rules and the terminals section).

--- src/org/eclipse/imp/lpg/editor/LPGOccurrenceMarker.java
+++ src/org/eclipse/imp/lpg/editor/LPGOccurrenceMarker.java
@@ -12,14 +12,21 @@
 
 package org.eclipse.imp.lpg.editor;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import lpg.runtime.IToken;
 
 import org.eclipse.imp.lpg.parser.ASTUtils;
 import org.eclipse.imp.lpg.parser.LPGParser.ASTNode;
 import org.eclipse.imp.lpg.parser.LPGParser.IASTNodeToken;
 import org.eclipse.imp.lpg.parser.LPGParser.JikesPG;
+import org.eclipse.imp.lpg.parser.LPGParser.nonTerm;
+import org.eclipse.imp.lpg.parser.LPGParser.terminal;
+import org.eclipse.imp.lpg.parser.LPGParser.terminal_symbol0;
 import org.eclipse.imp.parser.IParseController;
+import org.eclipse.imp.parser.SimpleLPGParseController;
 import org.eclipse.imp.services.IOccurrenceMarker;
 
 public class LPGOccurrenceMarker implements IOccurrenceMarker {
@@ -37,13 +44,66 @@ public class LPGOccurrenceMarker implements IOccurrenceMarker {
 	List<Object> result;
 
 	System.out.println();
+	
+
+	// Handles (I think) symbols in the terminals section
+	// (but not in the rules)
+	if (node instanceof terminal_symbol0) {
+		result = new ArrayList<Object>();
+		result.add(node);
+		String nodeString = node.toString();
+		if (parseController instanceof SimpleLPGParseController) {
+			SimpleLPGParseController lpgParseController = (SimpleLPGParseController) parseController;
+			ArrayList<IToken> tokens = lpgParseController.getParser().getParseStream().getTokens();
+			for (IToken t:  tokens) {
+				if (t.toString().equals(nodeString)) {
+					result.add(t);
+				}
+			}
+		}
+		return result;
+	}
+	
+	// SMS 30 Jul 2008:  Note:  it seems that everything that's in a rule
+	// ends up being represented as an AST node token, even if it's something
+	// that corresponds to a symbol
 	if (node instanceof IASTNodeToken) {
 	    IASTNodeToken nTok= (IASTNodeToken) node;
-	    result= Collections.singletonList(ASTUtils.findDefOf(nTok, root, parseController));
-	} else {
-	    result= Collections.emptyList();
+//	    result= Collections.singletonList(ASTUtils.findDefOf(nTok, root, parseController));
+
+	    Object def = ASTUtils.findDefOf(nTok, root, parseController);
+	    if (def == null) {
+	    	result = Collections.singletonList((Object)node);
+	    } else if (def instanceof nonTerm) {
+		    result = new ArrayList<Object>();
+		    result.add(def);
+		    result.addAll(ASTUtils.findRefsOf((nonTerm) def));
+	    } else if (def instanceof terminal) {
+		    result = new ArrayList<Object>();
+		    result.add(def);
+		    result.addAll(findAllOccurrences((terminal)def));
+	    } else {
+		    result = new ArrayList<Object>();
+	    	result.add(node);
+	    	result.add(def);
+	    }
+	    return result;
 	}
-	System.out.println("LPG occurrences = " + result);
+	
+    result= Collections.emptyList();
+//	System.out.println("LPG occurrences = " + result);
 	return result;
     }
+    
+    
+    public List<Object> findAllOccurrences(terminal def) {
+    	List<ASTNode> refs = ASTUtils.findRefsOf(def);
+    	List<Object> result = new ArrayList<Object>();
+    	result.addAll(refs);
+    	return result;
+    }
+    
+    
+    
+    
 }
