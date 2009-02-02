60bce989a8fdd3d898f3e2109d16d55979b1aef0
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-02-02 19:04:42 +0000

    Reformatted for sanity's sake.

--- src/org/eclipse/imp/lpg/builder/LPGNature.java
+++ src/org/eclipse/imp/lpg/builder/LPGNature.java
@@ -1,18 +1,14 @@
 /*******************************************************************************
-* Copyright (c) 2007 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ *******************************************************************************/
 
-*******************************************************************************/
-
-/*
- * Created on Nov 1, 2005
- */
 package org.eclipse.imp.lpg.builder;
 
 import org.eclipse.core.resources.IProject;
@@ -23,14 +19,14 @@ import org.eclipse.imp.runtime.IPluginLog;
 import org.eclipse.imp.smapifier.builder.SmapiProjectNature;
 
 public class LPGNature extends ProjectNatureBase {
-    public static final String	k_natureID = LPGRuntimePlugin.kPluginID + ".LPGNature";
+    public static final String k_natureID= LPGRuntimePlugin.kPluginID + ".LPGNature";
 
     public String getNatureID() {
-	return k_natureID;
+        return k_natureID;
     }
 
     public String getBuilderID() {
-	return LPGBuilder.BUILDER_ID;
+        return LPGBuilder.BUILDER_ID;
     }
 
     public void addToProject(IProject project) {
@@ -39,14 +35,14 @@ public class LPGNature extends ProjectNatureBase {
     }
 
     protected void refreshPrefs() {
-	// TODO implement preferences and hook in here
+        // TODO implement preferences and hook in here
     }
 
     public IPluginLog getLog() {
-	return LPGRuntimePlugin.getInstance();
+        return LPGRuntimePlugin.getInstance();
     }
 
     protected String getDownstreamBuilderID() {
-	return "org.eclipse.jdt.core.javabuilder";
+        return "org.eclipse.jdt.core.javabuilder";
     }
 }
