c72bb98814e73e72b13697898c1651ec91201125
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-03-12 13:29:11 +0000

    Reformatted to use spaces rather than tabs.

--- src/org/eclipse/imp/lpg/editor/LPGOccurrenceMarker.java
+++ src/org/eclipse/imp/lpg/editor/LPGOccurrenceMarker.java
@@ -1,14 +1,13 @@
 /*******************************************************************************
-* Copyright (c) 2007 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
-
-*******************************************************************************/
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ *******************************************************************************/
 
 package org.eclipse.imp.lpg.editor;
 
@@ -35,75 +34,72 @@ public class LPGOccurrenceMarker implements IOccurrenceMarker {
     }
 
     public String getKindName() {
-	return "References";
+        return "References";
     }
 
-    public List<Object> getOccurrencesOf(IParseController parseController, Object entity) {
-	ASTNode node= (ASTNode) entity;
-	JikesPG root= (JikesPG) parseController.getCurrentAst();
-	List<Object> result;
+    public List<Object> getOccurrencesOf(IParseController parseController,
+            Object entity) {
+        ASTNode node= (ASTNode) entity;
+        JikesPG root= (JikesPG) parseController.getCurrentAst();
+        List<Object> result;
 
-	System.out.println();
-	
+        System.out.println();
 
-	// Handles (I think) symbols in the terminals section
-	// (but not in the rules)
-	if (node instanceof terminal_symbol0) {
-		result = new ArrayList<Object>();
-		result.add(node);
-		String nodeString = node.toString();
-		if (parseController instanceof SimpleLPGParseController) {
-			SimpleLPGParseController lpgParseController = (SimpleLPGParseController) parseController;
-			ArrayList<IToken> tokens = lpgParseController.getParser().getParseStream().getTokens();
-			for (IToken t:  tokens) {
-				if (t.toString().equals(nodeString)) {
-					result.add(t);
-				}
-			}
-		}
-		return result;
-	}
-	
-	// SMS 30 Jul 2008:  Note:  it seems that everything that's in a rule
-	// ends up being represented as an AST node token, even if it's something
-	// that corresponds to a symbol
-	if (node instanceof IASTNodeToken) {
-	    IASTNodeToken nTok= (IASTNodeToken) node;
-//	    result= Collections.singletonList(ASTUtils.findDefOf(nTok, root, parseController));
+        // Handles (I think) symbols in the terminals section
+        // (but not in the rules)
+        if (node instanceof terminal_symbol0) {
+            result= new ArrayList<Object>();
+            result.add(node);
+            String nodeString= node.toString();
+            if (parseController instanceof SimpleLPGParseController) {
+                SimpleLPGParseController lpgParseController= (SimpleLPGParseController) parseController;
+                ArrayList<IToken> tokens= lpgParseController.getParser().getParseStream().getTokens();
+                for(IToken t : tokens) {
+                    if (t.toString().equals(nodeString)) {
+                        result.add(t);
+                    }
+                }
+            }
+            return result;
+        }
 
-	    Object def = ASTUtils.findDefOf(nTok, root, parseController);
-	    if (def == null) {
-	    	result = Collections.singletonList((Object)node);
-	    } else if (def instanceof nonTerm) {
-		    result = new ArrayList<Object>();
-		    result.add(def);
-		    result.addAll(ASTUtils.findRefsOf((nonTerm) def));
-	    } else if (def instanceof terminal) {
-		    result = new ArrayList<Object>();
-		    result.add(def);
-		    result.addAll(findAllOccurrences((terminal)def));
-	    } else {
-		    result = new ArrayList<Object>();
-	    	result.add(node);
-	    	result.add(def);
-	    }
-	    return result;
-	}
-	
-    result= Collections.emptyList();
-//	System.out.println("LPG occurrences = " + result);
-	return result;
+        // SMS 30 Jul 2008: Note: it seems that everything that's in a rule
+        // ends up being represented as an AST node token, even if it's
+        // something that corresponds to a symbol
+        if (node instanceof IASTNodeToken) {
+            IASTNodeToken nTok= (IASTNodeToken) node;
+            // result= Collections.singletonList(ASTUtils.findDefOf(nTok, root,
+            // parseController));
+
+            Object def= ASTUtils.findDefOf(nTok, root, parseController);
+            if (def == null) {
+                result= Collections.singletonList((Object) node);
+            } else if (def instanceof nonTerm) {
+                result= new ArrayList<Object>();
+                result.add(def);
+                result.addAll(ASTUtils.findRefsOf((nonTerm) def));
+            } else if (def instanceof terminal) {
+                result= new ArrayList<Object>();
+                result.add(def);
+                result.addAll(findAllOccurrences((terminal) def));
+            } else {
+                result= new ArrayList<Object>();
+                result.add(node);
+                result.add(def);
+            }
+            return result;
+        }
+
+        result= Collections.emptyList();
+        // System.out.println("LPG occurrences = " + result);
+        return result;
     }
-    
-    
+
     public List<Object> findAllOccurrences(terminal def) {
-    	List<ASTNode> refs = ASTUtils.findRefsOf(def);
-    	List<Object> result = new ArrayList<Object>();
-    	result.addAll(refs);
-    	return result;
+        List<ASTNode> refs= ASTUtils.findRefsOf(def);
+        List<Object> result= new ArrayList<Object>();
+        result.addAll(refs);
+        return result;
     }
-    
-    
-    
-    
+
 }
