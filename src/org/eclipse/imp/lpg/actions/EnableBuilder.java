d3bedd8dbf9439737526e9dd3df89b33f4bf8a9a
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-03-12 13:28:11 +0000

    Reformatted to use spaces rather than tabs.

--- src/org/eclipse/imp/lpg/actions/EnableBuilder.java
+++ src/org/eclipse/imp/lpg/actions/EnableBuilder.java
@@ -1,14 +1,13 @@
 /*******************************************************************************
-* Copyright (c) 2007 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
-
-*******************************************************************************/
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ *******************************************************************************/
 
 package org.eclipse.imp.lpg.actions;
 
@@ -31,19 +30,19 @@ public class EnableBuilder implements IWorkbenchWindowActionDelegate {
     public void init(IWorkbenchWindow window) {}
 
     public void run(IAction action) {
-	new LPGNature().addToProject(fProject);
+        new LPGNature().addToProject(fProject);
     }
 
     public void selectionChanged(IAction action, ISelection selection) {
-	if (selection instanceof IStructuredSelection) {
-	    IStructuredSelection ss= (IStructuredSelection) selection;
-	    Object first= ss.getFirstElement();
+        if (selection instanceof IStructuredSelection) {
+            IStructuredSelection ss= (IStructuredSelection) selection;
+            Object first= ss.getFirstElement();
 
-	    if (first instanceof IProject) {
-		fProject= (IProject) first;
-	    } else if (first instanceof IJavaProject) {
-		fProject= ((IJavaProject) first).getProject();
-	    }
-	}
+            if (first instanceof IProject) {
+                fProject= (IProject) first;
+            } else if (first instanceof IJavaProject) {
+                fProject= ((IJavaProject) first).getProject();
+            }
+        }
     }
 }
