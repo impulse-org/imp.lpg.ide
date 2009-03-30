325a96110c5b00f3addabb559259b1d82598a3b3
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-03-30 18:03:17 +0000

    Accommodate rename of IParseController.getNodeLocator() -> getSourcePositionLocator().

--- src/org/eclipse/imp/lpg/refactoring/InlineNonTerminalRefactoring.java
+++ src/org/eclipse/imp/lpg/refactoring/InlineNonTerminalRefactoring.java
@@ -82,7 +82,7 @@ public class InlineNonTerminalRefactoring extends Refactoring {
 	Point sel= editor.getSelection();
 	IParseController parseController= editor.getParseController();
 	ASTNode root= (ASTNode) parseController.getCurrentAst();
-	ISourcePositionLocator locator= parseController.getNodeLocator();
+	ISourcePositionLocator locator= parseController.getSourcePositionLocator();
 
 	return (ASTNode) locator.findNode(root, sel.x);
     }
